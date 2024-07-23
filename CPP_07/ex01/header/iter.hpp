bfd780099064e796e762610d145828b9c43d8d34
DjoykeAbyah <crudendjoyke@gmail.com>
2024-07-23 18:41:55 +0200

    iter for string issues

--- CPP_07/ex01/header/iter.hpp
+++ CPP_07/ex01/header/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: dreijans <dreijans@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/07/23 16:03:24 by dreijans      #+#    #+#                 */
-/*   Updated: 2024/07/23 18:11:38 by dreijans      ########   odam.nl         */
+/*   Updated: 2024/07/23 18:41:01 by dreijans      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,17 @@
 #include <iostream>
 
 template<typename T>
-void iter(T *address, T lenght, T *()) {
+void iter(T *address, T lenght, void(*func)(T*, int)) {
 	std::cout << "address is " << &address << std::endl;
 	std::cout << "lenght is " << lenght << std::endl;
-	std::cout << "function result is" << *() << std::endl;
+	std::cout << "function result is ";
+	func(address, lenght);
+	std::cout << std::endl;
 }
 
 template<typename T>
-T showArray(T *array, T lenght) {
-	for (int i = 0; i < lenght; i++)
-		std::cout << array[i] << std::endl;
+void showArray(T *array, int size) {
+	for (int i = 0; i < size; i++)
+		std::cout << array[i] << " ";
+	std::cout << std::endl;
 }
--- CPP_07/ex01/src/main.cpp
+++ CPP_07/ex01/src/main.cpp
@@ -6,12 +6,18 @@
 /*   By: dreijans <dreijans@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/07/17 16:48:18 by dreijans      #+#    #+#                 */
-/*   Updated: 2024/07/23 18:11:52 by dreijans      ########   odam.nl         */
+/*   Updated: 2024/07/23 18:41:20 by dreijans      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
 
 int main() {
+	int intArray[] = {1, 2, 3};
+	char charArray[] = {'a', 'b', 'c'};
+	const char *stringArray[] = {"one", "two", "three"};
 	
+	iter<int>(intArray, sizeof(intArray), showArray);
+	iter<char>(charArray, sizeof(charArray), showArray);
+	iter<char *>(stringArray, sizeof(stringArray), showArray);
 }
