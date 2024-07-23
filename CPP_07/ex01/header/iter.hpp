b46654e5fb569a39abfc9009d606d69dc752614d
DjoykeAbyah <crudendjoyke@gmail.com>
2024-07-23 18:12:14 +0200

    start test main

--- CPP_07/ex01/header/iter.hpp
+++ CPP_07/ex01/header/iter.hpp
@@ -1,17 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   main.cpp                                           :+:    :+:            */
+/*   iter.hpp                                           :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: dreijans <dreijans@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2024/07/17 16:48:18 by dreijans      #+#    #+#                 */
-/*   Updated: 2024/07/23 18:11:52 by dreijans      ########   odam.nl         */
+/*   Created: 2024/07/23 16:03:24 by dreijans      #+#    #+#                 */
+/*   Updated: 2024/07/23 18:11:38 by dreijans      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "iter.hpp"
+#pragma once
 
-int main() {
-	
+#include <iostream>
+
+template<typename T>
+void iter(T *address, T lenght, T *()) {
+	std::cout << "address is " << &address << std::endl;
+	std::cout << "lenght is " << lenght << std::endl;
+	std::cout << "function result is" << *() << std::endl;
+}
+
+template<typename T>
+T showArray(T *array, T lenght) {
+	for (int i = 0; i < lenght; i++)
+		std::cout << array[i] << std::endl;
 }
