dfcfd4df04d4a40ec44890aa855508d1e60c0a83
DjoykeAbyah <crudendjoyke@gmail.com>
2024-07-31 18:37:31 +0200

    continue with ex01

--- CPP_08/ex01/header/Span.hpp
+++ CPP_08/ex01/header/Span.hpp
@@ -6,13 +6,14 @@
 /*   By: dreijans <dreijans@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/07/31 15:54:44 by dreijans      #+#    #+#                 */
-/*   Updated: 2024/07/31 16:28:38 by dreijans      ########   odam.nl         */
+/*   Updated: 2024/07/31 18:37:11 by dreijans      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 #include <iostream>
+#include <list>
 
 class Span {
 	
