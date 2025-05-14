14dd2caa5450f9b72eb008b13b46f08bc6af1eff
DjoykeReijans <crudendjoyke@gmail.com>
2025-05-14 22:31:05 +0200

    adjust makefile

--- CPP_09/ex00/src/main.cpp
+++ CPP_09/ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: dreijans <dreijans@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/08/27 16:16:50 by dreijans      #+#    #+#                 */
-/*   Updated: 2025/05/14 22:18:48 by djoyke        ########   odam.nl         */
+/*   Updated: 2025/05/14 22:27:32 by djoyke        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,14 @@
 #include "BitcoinExchange.hpp"
 
 int main() {
-	BitcoinExchange BitcoinExchange;
+	BitcoinExchange bitcoinExchange;
 	
 	std::string filename = "data.csv";
-	BitcoinExchange.parseCsv(filename);
+	bitcoinExchange.parseCsv(filename);
 	
-	try {
-		
+	const std::map<std::string, float>& data = bitcoinExchange.getCsvData();
+
+	for (const auto& pair : data) {
+		std::cout << pair.first << "key: " << pair.second << "value: " << std::endl;
 	}
 }
