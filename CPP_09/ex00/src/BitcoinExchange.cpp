0abf28db583db7d4c4088825368a9a23f10224e8
DjoykeReijans <crudendjoyke@gmail.com>
2025-05-14 20:16:43 +0200

    start reading from input txt

--- CPP_09/ex00/src/BitcoinExchange.cpp
+++ CPP_09/ex00/src/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: dreijans <dreijans@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/08/27 16:17:04 by dreijans      #+#    #+#                 */
-/*   Updated: 2025/05/14 19:53:37 by djoyke        ########   odam.nl         */
+/*   Updated: 2025/05/14 20:16:17 by djoyke        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ void BitcoinExchange::parseCsv(std::string &filename) {
 	
 	//read from file to multimap
 	std::string line;
-	std::getline(file, line);
+	std::getline(file, line); //skips first line header
 
 	while (std::getline(file, line)) {
 		std::stringstream stringStream(line);			//turn the line into a stream
@@ -43,13 +43,34 @@ void BitcoinExchange::parseCsv(std::string &filename) {
 
 		if (std::getline(stringStream, date, ',') && std::getline(stringStream, valueString)){
 			std::stringstream(valueString) >> value; 	//converts string into float, reads from stream into value using >>
-			csvdata[date] = value;
+			csvdata[date] = value; //lookup and insertion if not exists
 		}
 	}
-
 	file.close();
 }
 
 void BitcoinExchange::readTxt(std::string &filename) {
+	std::fstream file(filename);
+	if (!file.is_open())
+		throw OpenFileException();
 	
+	std::string line;
+	std::getline(file, line); //skips first line
+
+	while (std::getline(file, line)) {
+		std::stringstream stringStream(line);			//turn the line into a stream
+		std::string date;
+		std::string valueString;
+		
+		if (!std::getline(stringStream, date, '|') || !std::getline(stringStream, valueString)) {
+			std::cerr << "Bad input : " << line << std::endl; 
+			continue; // should I continue?
+		}
+
+		//Trim whitespace
+		//convert value to float
+		//check range
+		//lookup exchange rate in csv data
+	}
+	file.close();
 }
