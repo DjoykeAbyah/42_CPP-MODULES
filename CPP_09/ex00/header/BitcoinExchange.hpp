59963f7921a579c53c225d3b40700173436172bf
DjoykeReijans <crudendjoyke@gmail.com>
2025-06-06 11:03:53 +0200

    removed todo

--- CPP_09/ex00/header/BitcoinExchange.hpp
+++ CPP_09/ex00/header/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: dreijans <dreijans@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/08/27 16:15:36 by dreijans      #+#    #+#                 */
-/*   Updated: 2025/05/14 22:13:26 by djoyke        ########   odam.nl         */
+/*   Updated: 2025/06/06 11:03:32 by djoyke        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,25 +17,6 @@
 #include <exception>
 #include <sstream>
 
-//program name must be btc?
-//take a file as argument
-//each line must use format "date | value"
-//valid date format Year-Month-day
-//valid value float or positive int between 0 and 100
-//if date used in input does not exist use closest lower date in database
-//you cannot use the container you've used in this exercise for another
-
-//undordered multipmap
-//https://cplusplus.com/reference/unordered_map/unordered_multimap/
-
-/**
- * @todo reconsider if unordered multipmap is the way to go to prevent hasing to the same key?
- * making the map a private attribute aligns with OOP principles like encapsulation, 
- * abstraction, and data hiding, which help create a more robust, 
- * maintainable, and user-friendly class. 
- * However, if the specific requirements of your program favor returning the map directly, 
- * that approach can also be justified, especially for simpler use cases.
- */
 class BitcoinExchange {
 	
 	private:
--- CPP_09/ex00/src/BitcoinExchange.cpp
+++ CPP_09/ex00/src/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: dreijans <dreijans@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/08/27 16:17:04 by dreijans      #+#    #+#                 */
-/*   Updated: 2025/05/15 21:50:50 by djoyke        ########   odam.nl         */
+/*   Updated: 2025/06/06 11:03:42 by djoyke        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,12 +68,12 @@ void BitcoinExchange::readTxt(std::string &filename) {
 		
 		if (!std::getline(stringStream, date, '|') || !std::getline(stringStream, valueString)) {
 			std::cerr << "Bad input : " << line << std::endl; 
-			continue; // TODO should I continue?
+			continue;
 		}
 
 		//Trim whitespace
 		date.erase(0, date.find_first_not_of(" \t"));
-		date.erase(date.find_last_not_of(" \t") + 1); // TODO is +1 neccesary?
+		date.erase(date.find_last_not_of(" \t") + 1);
 		valueString.erase(0, valueString.find_first_not_of("\t"));
 		valueString.erase(valueString.find_last_not_of(" \t") + 1);
 		
